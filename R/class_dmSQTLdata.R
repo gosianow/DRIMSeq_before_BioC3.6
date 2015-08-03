852d77fb46d917c3ecfc8bd6178aee0725e8aab6
Malgorzata Nowicka <gosia.nowicka@uzh.ch>
2015-08-03 10:12:37 +0200

    DM_0.1.7 SQTL: initiate s4 classes in SQTL analysis (see previous commit as well)

---
+++ R/class_dmSQTLdata.R
@@ -0,0 +1,163 @@
+setClass("dmSQTLdata", 
+  representation(counts = "MatrixList", genotypes = "MatrixList", samples = "DataFrame"))
+
+
+
+dmSQTLdata <- function(counts, gene_id_counts, feature_id_counts, genotypes, gene_id_genotypes, snp_id_genotypes, sample_id){
+  
+  stopifnot( class( counts ) %in% c("matrix"))
+  stopifnot( mode( counts ) %in% c("numeric"))
+  counts <- ceiling(counts)
+  
+  stopifnot( class( gene_id_counts ) %in% c("character"))
+  stopifnot( class( feature_id_counts ) %in% c("character"))
+  stopifnot( length(gene_id_counts) == length( feature_id_counts ) )
+
+  stopifnot( class( genotypes ) %in% c("matrix"))
+  
+  stopifnot( class( gene_id_genotypes ) %in% c("character"))
+  stopifnot( class( snp_id_genotypes ) %in% c("character"))
+  stopifnot( length(gene_id_genotypes) == length( snp_id_genotypes ) )
+  
+  stopifnot( class( sample_id ) %in% c("character"))
+  
+  ### keep genes that are in counts and in genotypes
+  
+  genes2keep <- gene_id_counts %in% gene_id_genotypes
+  counts <- counts[genes2keep, , drop = FALSE]
+  gene_id_counts <- gene_id_counts[genes2keep]
+  feature_id_counts <- feature_id_counts[genes2keep]
+
+  genes2keep <- gene_id_genotypes %in% gene_id_counts
+  genotypes <- genotypes[genes2keep, , drop = FALSE]
+  gene_id_genotypes <- gene_id_genotypes[genes2keep]
+  snp_id_genotypes <- snp_id_genotypes[genes2keep]
+  
+  ### order genes in genotypes as in counts
+  
+  gene_id_genotypes <- factor(gene_id_genotypes, levels = unique(gene_id_counts))
+  gene_id_counts <- factor(gene_id_counts, levels = unique(gene_id_counts))
+  
+  
+  order_genotypes <- order(gene_id_genotypes)
+  genotypes <- genotypes[order_genotypes, , drop = FALSE]
+  gene_id_genotypes <- gene_id_genotypes[order_genotypes]
+  snp_id_genotypes <- snp_id_genotypes[order_genotypes]
+  
+  colnames(counts) <- sample_id
+  rownames(counts) <- feature_id_counts
+  
+  colnames(genotypes) <- sample_id
+  rownames(genotypes) <- snp_id_genotypes
+  
+  counts <- new( "MatrixList", unlistData = counts, partitioning = PartitioningByEnd(as.numeric(gene_id_counts), NG = nlevels(gene_id_counts), names = levels(gene_id_counts)) )
+  
+  genotypes <- new( "MatrixList", unlistData = genotypes, partitioning = PartitioningByEnd(as.numeric(gene_id_genotypes), NG = nlevels(gene_id_genotypes), names = levels(gene_id_genotypes)) )
+  
+  samples <- DataFrame(sample_id = sample_id)
+  
+  data <- new("dmSQTLdata", counts = counts, genotypes = genotypes, samples = samples)
+  
+  return(data)
+  
+}
+
+
+
+dmSQTLdataFromRanges <- function(counts, gene_id_counts, feature_id_counts, gene_ranges, genotypes, snp_id_genotypes, snp_ranges, sample_id, window = 5e3){
+
+  rownames(genotypes) <- snp_id_genotypes
+  gene_ranges <- GenomicRanges::resize(gene_ranges, GenomicRanges::width(gene_ranges) + 2 * window, fix = "center")
+
+  ## Match genes and SNPs
+  variantMatch <- GenomicRanges::findOverlaps(gene_ranges, snp_ranges, select = "all")
+
+  q <- queryHits(variantMatch)
+  s <- subjectHits(variantMatch)
+
+  genotypes <- genotypes[s, ]
+  snp_id_genotypes <- snp_id_genotypes[s]
+  gene_id_genotypes <- names(gene_ranges)[q]
+
+
+  data <- dmSQTLdata(counts = counts, gene_id_counts = gene_id_counts, feature_id_counts = feature_id_counts, genotypes = genotypes, gene_id_genotypes = gene_id_genotypes, snp_id_genotypes = snp_id_genotypes, sample_id = sample_id)
+
+  return(data)
+
+}
+
+
+setMethod("show", "dmSQTLdata", function(object){
+  
+  cat("An object of class", class(object), "\n")
+  
+  cat("Slot \"counts\":\n")
+  print(object@counts)
+  
+  cat("\nSlot \"genotypes\":\n")
+  print(object@genotypes)
+  
+  cat("\nSlot \"samples\":\n")
+  print(object@samples)
+  
+  })
+
+
+
+
+setMethod("names", "dmSQTLdata", function(x) names(data@counts) )
+
+
+setGeneric("dmSQTLfilter", function(x, ...) standardGeneric("dmSQTLfilter"))
+
+
+setMethod("dmSQTLfilter", "dmSQTLdata", function(x, min_samps_gene_expr = 70, min_gene_expr = 1, min_samps_feature_prop = 5, min_feature_prop = 0.1, max_features = Inf, minor_allel_freq = 0.05, BPPARAM = MulticoreParam(workers = 1)){
+  
+  
+  data_filtered <- dmSQTL_filter(counts = x@counts, genotypes = x@genotypes, samples = x@samples, min_samps_gene_expr = min_samps_gene_expr, min_gene_expr = min_gene_expr, min_samps_feature_prop = min_samps_feature_prop, min_feature_prop = min_feature_prop, max_features = max_features, minor_allel_freq = minor_allel_freq, BPPARAM = BPPARAM)
+  
+  return(data_filtered)
+  
+  
+  })
+
+
+
+setGeneric("dmSQTLplotData", function(x, ...) standardGeneric("dmSQTLplotData"))
+
+setMethod("dmSQTLplotData", "dmSQTLdata", function(x, out_dir = NULL){
+  
+ dmSQTL_plotData(counts = x@counts, genotypes = x@genotypes, out_dir = out_dir)
+  
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/class_dmSQTLdispersion.R
@@ -0,0 +1,158 @@
+setClassUnion("CompressedNumericListORNULL", c("CompressedNumericList", "NULL"))
+
+setClass("dmSQTLdispersion", 
+  contains = "dmSQTLdata",
+  representation(mean_expression = "numericORNULL", 
+    common_dispersion = "numericORNULL",
+    tagwise_dispersion = "CompressedNumericListORNULL"), 
+  prototype(mean_expression = NULL,
+    common_dispersion = NULL, 
+    tagwise_dispersion = NULL))
+
+
+
+setMethod("show", "dmSQTLdispersion", function(object){
+      
+  callNextMethod(object)
+  
+  cat("\nSlot \"mean_expression\":\n")
+  show_numeric(object@mean_expression)
+  
+  cat("\nSlot \"common_dispersion\":\n")
+  show_numeric(object@common_dispersion)
+  
+  cat("\nSlot \"tagwise_dispersion\":\n")
+  print(object@tagwise_dispersion)
+  
+  })
+
+
+
+setGeneric("dmSQTLdispersion", function(x, ...) standardGeneric("dmSQTLdispersion"))
+
+setMethod("dmSQTLdispersion", "dmSQTLdata", function(x, mean_expression = TRUE, common_dispersion = FALSE, tagwise_dispersion = TRUE, disp_adjust = TRUE, disp_mode = c("optimize", "optim", "constrOptim", "grid")[4], disp_interval = c(0, 1e+5), disp_tol = 1e-08, disp_init = 100, disp_init_weirMoM = TRUE, disp_grid_length = 21, disp_grid_range = c(-10, 10), disp_moderation = c("none", "common", "trended")[1], disp_prior_df = 10, disp_span = 0.3, prop_mode = c( "constrOptim", "constrOptimG", "FisherScoring")[2], prop_tol = 1e-12, verbose = FALSE, BPPARAM = MulticoreParam(workers = 1)){
+   
+  if(mean_expression){
+    mean_expression <- dm_estimateMeanExpression(counts = x@counts, BPPARAM = BPPARAM)
+    }else{
+      mean_expression <- NULL
+    }
+  
+  if(common_dispersion){
+    common_dispersion <- dmDS_estimateCommonDispersion(counts = x@counts, samples = x@samples, disp_adjust = disp_adjust, disp_interval = disp_interval, disp_tol = 1e+01, prop_mode = prop_mode, prop_tol = prop_tol, verbose = verbose, BPPARAM = BPPARAM)
+    }else{
+      common_dispersion <- NULL
+    }
+
+  
+  if(tagwise_dispersion){
+    
+    if(disp_mode == "grid" && !is.null(common_dispersion)){
+      cat("!Using common dispersion as disp_init in 'grid' mode!\n")
+      disp_init <- common_dispersion
+    }
+
+    tagwise_dispersion <- dmDS_estimateTagwiseDispersion(counts = x@counts, samples = x@samples, disp_adjust = disp_adjust, disp_mode = disp_mode, disp_interval = disp_interval, disp_tol = disp_tol, disp_init = disp_init, disp_init_weirMoM = disp_init_weirMoM, disp_grid_length = disp_grid_length, disp_grid_range = disp_grid_range, disp_moderation = disp_moderation, disp_prior_df = disp_prior_df, disp_span = disp_span, prop_mode = prop_mode, prop_tol = prop_tol, verbose = verbose, BPPARAM = BPPARAM)
+    
+  }else{
+    tagwise_dispersion <- NULL
+  }
+  
+  
+  return(new("dmDSdispersion", mean_expression = mean_expression, common_dispersion = common_dispersion, tagwise_dispersion = tagwise_dispersion, counts = x@counts, samples = x@samples))
+  
+  
+  })
+
+
+
+setMethod("dmDSdispersion", "dmDSdispersion", function(x, mean_expression = FALSE, common_dispersion = FALSE, tagwise_dispersion = TRUE, disp_adjust = TRUE, disp_mode = c("optimize", "optim", "constrOptim", "grid")[4], disp_interval = c(0, 1e+5), disp_tol = 1e-08, disp_init = 100, disp_init_weirMoM = TRUE, disp_grid_length = 21, disp_grid_range = c(-10, 10), disp_moderation = c("none", "common", "trended")[1], disp_prior_df = 10, disp_span = 0.3, prop_mode = c( "constrOptim", "constrOptimG", "FisherScoring")[2], prop_tol = 1e-12, verbose = FALSE, BPPARAM = MulticoreParam(workers=1)){
+   
+  if(mean_expression){
+    mean_expression <- dm_estimateMeanExpression(counts = x@counts, BPPARAM = BPPARAM)
+    }else{
+      mean_expression <- x@mean_expression
+    }
+  
+  if(common_dispersion){
+    common_dispersion <- dmDS_estimateCommonDispersion(counts = x@counts, samples = x@samples, disp_adjust = disp_adjust, disp_interval = disp_interval, disp_tol = 1e+01, prop_mode = prop_mode, prop_tol = prop_tol, verbose = verbose, BPPARAM = BPPARAM)
+    }else{
+      common_dispersion <- x@common_dispersion
+    }
+
+  
+  if(tagwise_dispersion){
+    
+    if(disp_mode == "grid" && !is.null(common_dispersion)){
+      cat("!Using common_dispersion =", round(common_dispersion, 2), "as disp_init in 'grid' mode!\n")
+      disp_init <- common_dispersion
+    }
+
+    tagwise_dispersion <- dmDS_estimateTagwiseDispersion(counts = x@counts, samples = x@samples, disp_adjust = disp_adjust, disp_mode = disp_mode, disp_interval = disp_interval, disp_tol = disp_tol, disp_init = disp_init, disp_init_weirMoM = disp_init_weirMoM, disp_grid_length = disp_grid_length, disp_grid_range = disp_grid_range, disp_moderation = disp_moderation, disp_prior_df = disp_prior_df, disp_span = disp_span, prop_mode = prop_mode, prop_tol = prop_tol, verbose = verbose, BPPARAM = BPPARAM)
+    
+  }else{
+    tagwise_dispersion <- x@tagwise_dispersion
+  }
+  
+  
+  return(new("dmDSdispersion", mean_expression = mean_expression, common_dispersion = common_dispersion, tagwise_dispersion = tagwise_dispersion, counts = x@counts, samples = x@samples))
+  
+  
+  })
+
+
+
+
+setGeneric("dmDSplotDispersion", function(x, ...) standardGeneric("dmDSplotDispersion"))
+
+setMethod("dmDSplotDispersion", "dmDSdispersion", function(x, out_dir = NULL){
+  
+  # tagwise_dispersion = x@tagwise_dispersion; mean_expression = x@mean_expression; nr_features = width(x@counts@partitioning); common_dispersion = x@common_dispersion
+  
+  dmDS_plotDispersion(tagwise_dispersion = x@tagwise_dispersion, mean_expression = x@mean_expression, nr_features = width(x@counts@partitioning), common_dispersion = x@common_dispersion, out_dir = out_dir)
+  
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
