eceb0277b0fa70f84002a286eebd9170bce031c6
Malgorzata Nowicka <gosia.nowicka@uzh.ch>
2016-04-05 14:21:50 +0200

    DRIMSeq_0.3.3: Do NOT calculate deviance; dev <- NA

--- R/dm_fitOneGeneOneGroup.R
+++ R/dm_fitOneGeneOneGroup.R
@@ -35,7 +35,8 @@ dm_fitOneGeneOneGroup <- function(y, gamma0, prop_mode = c("constrOptim", "const
     
     df <- k - 1
     lik <- dm_likG(pi = pi_init[-k], gamma0 = gamma0, y = y) # likelihood
-    dev <- dm_devG(pi = pi_init[-k], gamma0 = gamma0, y = y) # deviance
+    dev <- NA
+    # dev <- dm_devG(pi = pi_init[-k], gamma0 = gamma0, y = y) # deviance
     
     stats <- c(lik, df, dev)
     names(stats) <- c("lik", "df", "dev")
@@ -91,7 +92,8 @@ dm_fitOneGeneOneGroup <- function(y, gamma0, prop_mode = c("constrOptim", "const
            co <- constrOptim(pi_init[-k], f = dm_lik, grad = dm_score, ui = ui, ci = ci, control = list(fnscale = -1, reltol = prop_tol), gamma0 = gamma0, y = y)
            
            pi <- co$par
-           dev <- dm_devG(pi = pi, gamma0 = gamma0, y = y) 
+           dev <- NA
+           # dev <- dm_devG(pi = pi, gamma0 = gamma0, y = y) 
            pi <- c(pi, 1-sum(pi))
            lik <- co$value
            
@@ -109,7 +111,8 @@ dm_fitOneGeneOneGroup <- function(y, gamma0, prop_mode = c("constrOptim", "const
            co <- constrOptim(pi_init[-k], f = dm_likG, grad = dm_scoreG, ui = ui, ci = ci, control = list(fnscale = -1, reltol = prop_tol), gamma0 = gamma0, y = y)
            
            pi <- co$par
-           dev <- dm_devG(pi = pi, gamma0 = gamma0, y = y)
+           dev <- NA
+           # dev <- dm_devG(pi = pi, gamma0 = gamma0, y = y)
            pi <- c(pi, 1-sum(pi))
            lik <- co$value
            
